5f9c1bf315774cedcc26bf5c222d355ed5ed876c
Adhicandoit <adhirajgarg515@gmail.com>
2025-10-16 19:36:47 +0530

    first-push

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavBar } from "./Components/nav-bar/nav-bar";
+import { MainMenu } from './Components/main-menu/main-menu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MainMenu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
