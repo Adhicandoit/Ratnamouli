cf04a1dab3727e3b1c10b7962566abb183c986a6
Adhicandoit <adhirajgarg515@gmail.com>
2025-10-18 15:48:07 +0530

    Fixed Tailwind config, removed navbar, verified build locally

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component, signal } from '@angular/core';
-import { NavBar } from "./Components/nav-bar/nav-bar";
 import { MainMenu } from './Components/main-menu/main-menu';
 
 @Component({
